c97e2a22ae0b53b6f9d21ea718a03d16b93257f9
Owen <512808529@qq.com>
2019-09-11 16:54:22 +0800

    第一天的代码，登录、路由守卫、退出登录

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem('token')
+      this.$router.push('/login')
+    }
+  },
+  created() {
+    this.$message.closeAll()
+    this.$message({
+      type: 'success',
+      showClose: true,
+      message: '成功进入后台管理系统'
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
